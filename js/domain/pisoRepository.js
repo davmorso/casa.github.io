4d13aedf9ddf3e0846619d7b419d91a3248d6836
Your Name <you@example.com>
2025-07-20 19:35:20 +0200

    version 27: retocado imagenes

--- js/domain/pisoRepository.js
+++ js/domain/pisoRepository.js
@@ -14,21 +14,23 @@ class PisoRepository {
         { src: './img/casa16.jpeg', alt: 'Primera planta: acceso segunda planta' }
       ],
       'segundo-piso': [
-        { src: './img/casa7.jpeg', alt: 'Segunda planta: guardarropia' },
         { src: './img/casa8.jpeg', alt: 'Segunda planta: pasillo suite' },
+        { src: './img/casa7.jpeg', alt: 'Segunda planta: guardarropia' },        
         { src: './img/casa11.jpeg', alt: 'Segunda planta: habitación' },
         { src: './img/casa12.jpeg', alt: 'Segunda planta: terraza habitación' },
-        { src: './img/casa13.jpeg', alt: 'Segunda planta: armario habitación' },
+        { src: './img/casa13.jpeg', alt: 'Segunda planta: baño habitación' },
         { src: './img/casa14.jpeg', alt: 'Segunda planta: baño habitación' },
-        { src: './img/casa15.jpeg', alt: 'Segunda planta: baño habitación' }
+        { src: './img/casa15.jpeg', alt: 'Segunda planta: baño habitación' },
+        { src: './img/casa31.jpeg', alt: 'Segunda planta: acceso tercera planta' }
       ],
       'tercer-piso': [
-        { src: './img/casa1.jpeg', alt: 'Tercera planta: habitación' },
-        { src: './img/casa2.jpeg', alt: 'Tercera planta: habitación' },
-        { src: './img/casa3.jpeg', alt: 'Tercera planta: baño' },
-        { src: './img/casa4.jpeg', alt: 'Tercera planta: terraza' },
-        { src: './img/casa5.jpeg', alt: 'Tercera planta: terraza' },
+        { src: './img/casa32.jpeg', alt: 'Tercera planta: habitación' },        
+        { src: './img/casa3.jpeg', alt: 'Tercera planta: habitación' },
+        { src: './img/casa4.jpeg', alt: 'Tercera planta: habitación' },
+        { src: './img/casa5.jpeg', alt: 'Tercera planta: habitación' },
         { src: './img/casa6.jpeg', alt: 'Tercera planta: escaleras' },
+        { src: './img/casa2.jpeg', alt: 'Tercera planta: terraza' },
+        { src: './img/casa33.jpeg', alt: 'Tercera planta: terraza vistas mar' },
         { src: './img/casa29.jpeg', alt: 'Tercera planta: baño' },
         { src: './img/casa30.jpeg', alt: 'Tercera planta: baño' }
       ]
