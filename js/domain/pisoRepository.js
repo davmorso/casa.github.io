41f1e2a8362671c85eef8fa7f491aeec909c1a0f
Your Name <you@example.com>
2025-07-20 18:42:51 +0200

    version 25: refractorizaciuon en DDD y SOLID

---
+++ js/domain/pisoRepository.js
@@ -0,0 +1,43 @@
+class PisoRepository {
+  constructor() {
+    this.pisos = {
+      'primer-piso': [
+        { src: './img/casa24.jpeg', alt: 'Primera planta: comedor.' },
+        { src: './img/casa18.jpeg', alt: 'Primera planta: lavadero / terraza' },
+        { src: './img/casa19.jpeg', alt: 'Primera planta: lavadero / terraza' },
+        { src: './img/casa20.jpeg', alt: 'Primera planta: vistas exterior' },
+        { src: './img/casa21.jpeg', alt: 'Primera planta: comedor' },
+        { src: './img/casa22.jpeg', alt: 'Primera planta: comedor' },
+        { src: './img/casa23.jpeg', alt: 'Primera planta: comedor' },
+        { src: './img/casa25.jpeg', alt: 'Primera planta: cocina' },
+        { src: './img/casa27.jpeg', alt: 'Primera planta: cocina' },
+        { src: './img/casa16.jpeg', alt: 'Primera planta: acceso segunda planta' }
+      ],
+      'segundo-piso': [
+        { src: './img/casa7.jpeg', alt: 'Segunda planta: guardarropia' },
+        { src: './img/casa8.jpeg', alt: 'Segunda planta: pasillo suite' },
+        { src: './img/casa11.jpeg', alt: 'Segunda planta: habitación' },
+        { src: './img/casa12.jpeg', alt: 'Segunda planta: terraza habitación' },
+        { src: './img/casa13.jpeg', alt: 'Segunda planta: armario habitación' },
+        { src: './img/casa14.jpeg', alt: 'Segunda planta: baño habitación' },
+        { src: './img/casa15.jpeg', alt: 'Segunda planta: baño habitación' }
+      ],
+      'tercer-piso': [
+        { src: './img/casa1.jpeg', alt: 'Tercera planta: habitación' },
+        { src: './img/casa2.jpeg', alt: 'Tercera planta: habitación' },
+        { src: './img/casa3.jpeg', alt: 'Tercera planta: baño' },
+        { src: './img/casa4.jpeg', alt: 'Tercera planta: terraza' },
+        { src: './img/casa5.jpeg', alt: 'Tercera planta: terraza' },
+        { src: './img/casa6.jpeg', alt: 'Tercera planta: baño' }
+      ]
+    };
+  }
+
+  getPlantas() {
+    return Object.keys(this.pisos);
+  }
+
+  getImagenes(planta) {
+    return this.pisos[planta] || [];
+  }
+}
